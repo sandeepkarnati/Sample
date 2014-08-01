ac302a9c948086fbfe279b5b93560c1e251454e7
sandeepkarnati <karnatisandeepkumar@gmail.com>
2014-08-01 23:39:08 +0530

    Edit

--- aa/src/com/example/aa/MainActivity.java
+++ aa/src/com/example/aa/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+// sample edit
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
